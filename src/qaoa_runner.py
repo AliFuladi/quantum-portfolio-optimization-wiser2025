0c1ca09e8a454896265d0a7fe558ea516af6c92d
Ali Fuladi <137862354+AliFuladi@users.noreply.github.com>
2025-11-26 16:34:26 +0100

    Refactor QaoaRunner class for clarity and efficiency

--- src/qaoa_runner.py
+++ src/qaoa_runner.py
@@ -8,41 +8,21 @@ from qiskit_optimization.algorithms import MinimumEigenOptimizer
 
 
 class QaoaRunner:
-    """
-    A class to run the QAOA algorithm on a given QUBO problem.
-    This version uses the modern Qiskit Sampler primitive (v1.0+ compatible).
-    """
+    """Small helper around QAOA + MinimumEigenOptimizer for a given QUBO."""
 
     def __init__(self, qubo: QuadraticProgram, num_assets: int):
         self.qubo = qubo
         self.num_assets = num_assets
-        # Use the modern Sampler primitive from qiskit_aer
         self.sampler = Sampler()
 
-    def run(self):
-        """
-        Executes the QAOA algorithm to solve the QUBO.
-
-        Returns:
-            np.array: The solution as a binary array.
-        """
-        optimizer = COBYLA()
-
-        # The QAOA solver
-        qaoa_mes = QAOA(
-            optimizer=optimizer,
-            sampler=self.sampler,
-            reps=1
-        )
-
-        # Use MinimumEigenOptimizer to solve the QUBO problem
+    def run(self) -> np.ndarray:
+        """Run QAOA once and return the binary solution."""
+        qaoa_mes = QAOA(optimizer=COBYLA(), sampler=self.sampler, reps=1)
         optimizer = MinimumEigenOptimizer(min_eigen_solver=qaoa_mes)
         result = optimizer.solve(self.qubo)
 
-        # The result.x contains the solution in a binary array format
-        # We need to make sure the solution has the correct length
         solution = np.zeros(self.num_assets)
-        for i in range(len(result.x)):
-            solution[i] = result.x[i]
-
+        for i, x in enumerate(result.x):
+            solution[i] = x
         return solution
+
